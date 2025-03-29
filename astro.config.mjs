893967ba292199c290aacfd3097e8f2da7e9bb66
DYlan <gownerbeats@gmail.com>
2025-03-28 23:41:32 -0500

    ds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -8,11 +9,11 @@ export default defineConfig({
   },
   output: 'server',
   adapter: vercel({
+    includeFiles: ['.env'],
+    maxDuration: 60,
     analytics: true,
-    // Configuramos Node.js 20 como runtime (compatible con Vercel)
     functionPerRoute: false,
     devImageService: true,
-    // Especificar runtime actualizado
     runtime: 'nodejs20.x',
   }),
 });
